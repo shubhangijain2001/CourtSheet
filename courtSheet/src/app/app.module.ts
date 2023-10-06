0933074fc6baed4cd8c4cbd4d9b700f23ce56190
Shubhangi <shubhangi2001jain@gmail.com>
2023-10-06 19:09:19 +0530

    section 1 created

--- courtSheet/src/app/app.module.ts
+++ courtSheet/src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BaseFormComponent } from './base-form/base-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DropdownModule} from 'primeng/dropdown';
+import { RequiredPipe } from './Pipes/required.pipe'
 @NgModule({
   declarations: [
     AppComponent,
     BaseFormComponent,
+    RequiredPipe,
     
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
