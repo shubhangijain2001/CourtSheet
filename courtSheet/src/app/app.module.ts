9503eb75f58c79302162468e2aa1ef1f5e85d2a3
Shubhangi <shubhangi2001jain@gmail.com>
2023-10-06 11:34:26 +0530

    base form component created

--- courtSheet/src/app/app.module.ts
+++ courtSheet/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { BaseFormComponent } from './base-form/base-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    BaseFormComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
